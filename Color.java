16d209eb1930800a8e5bb85045dd9f4d042176ff
Malihe Moradi <m.moradi.ps@gmail.com>
2019-04-17 21:13:43 +0430

    put all colors on git

---
+++ Color.java
@@ -0,0 +1,44 @@
+package com.hivatec.transitionapplication;
+
+import android.support.annotation.ColorInt;
+
+/**
+ * Created by Malihe Moradi in 17 april 2019
+ */
+public class Color {
+
+    /**
+     * Main colors
+     */
+    @ColorInt
+    public static final int BLACK = 0xFF000000;
+    @ColorInt
+    public static final int DKGRAY = 0xFF444444;
+    @ColorInt
+    public static final int GRAY = 0xFF888888;
+    @ColorInt
+    public static final int LTGRAY = 0xFFCCCCCC;
+    @ColorInt
+    public static final int WHITE = 0xFFFFFFFF;
+    @ColorInt
+    public static final int RED = 0xFFFF0000;
+    @ColorInt
+    public static final int GREEN = 0xFF00FF00;
+    @ColorInt
+    public static final int BLUE = 0xFF0000FF;
+    @ColorInt
+    public static final int YELLOW = 0xFFFFFF00;
+    @ColorInt
+    public static final int CYAN = 0xFF00FFFF;
+    @ColorInt
+    public static final int MAGENTA = 0xFFFF00FF;
+    @ColorInt
+    public static final int TRANSPARENT = 0;
+
+
+    /**
+     * Material colors
+     */
+
+
+}
